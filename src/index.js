1d6ef3e6083bbaeef840cc9e767566a8f2452de3
Bello S Babakolo <fleetbeekay@gmail.com>
2021-02-18 14:49:18 +0100

    Configure linters

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 
 function Calculator() {
   return (
-      <h1>Calculator App</h1>
+    <h1>Calculator App</h1>
   );
 }
 
 ReactDOM.render(
   <Calculator />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
