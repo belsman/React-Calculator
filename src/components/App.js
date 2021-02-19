e67e1bfc92bfb86f816beaaa7ea0e073b3023e7d
Bello S Babakolo <fleetbeekay@gmail.com>
2021-02-19 17:11:02 +0100

    Implement fragments

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 const App = () => (
-  <div className="container">
-    <Display result="0" />
-    <ButtonPanel />
-  </div>
+  <>
+    <div className="container">
+      <Display result="0" />
+      <ButtonPanel />
+    </div>
+  </>
 );
 
 export default App;
