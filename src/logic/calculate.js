b447e6037ddbd479dc409a98829109dc1091e376
Bello S Babakolo <fleetbeekay@gmail.com>
2021-02-23 09:05:39 +0100

    Complete the calc logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,65 @@
+import operate from "./operate";
+
 const calculate = (calcData, buttonName) => {
-  calcData.x = 1;
-  return calcData;
+  const { total, next, operator } = calcData;
+
+  if (!operator && !next) {
+    if (/\d/.test(buttonName)) {
+      total += buttonName;
+    }
+
+    if (buttonName === ".") {
+      if (total.indexOf(buttonName) === -1) {
+        total += buttonName;
+      }
+    }
+
+    if (buttonName === "+/-") {
+      total = operate(total, 1, "-");
+    }
+
+    if (buttonName === "%") {
+      total = operate(total, 0.01, "x");
+    }
+  }
+
+  if (operationList.includes(buttonName)) {
+    operator = buttonName;
+  }
+
+  if (total && operator) {
+    if (/\d/.test(buttonName)) {
+      next += buttonName;
+    }
+
+    if (buttonName === ".") {
+      if (next.indexOf(buttonName) === -1) {
+        next += buttonName;
+      }
+    }
+
+    if (buttonName === "+/-") {
+      next = operate(next, 1, "-");
+    }
+
+    if (buttonName === "%") {
+       next = operate(next, 0.01, "x");
+    }
+  }
+
+  if (total && operator && next) {
+    result = operate(total, next, operator);
+    next = "";
+    operator = "";
+  }
+
+  if (buttonName === "AC") {
+    result = "0";
+    next = 0;
+    operator = 0;
+  }
+
+  return { total, next, operator };
 };
 
 export default calculate;
