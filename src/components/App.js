030bec083eb8dccd70179d5f26c80868ba08de5e
Bello Babakolo <fleetbeekay@gmail.com>
2021-02-24 06:11:09 +0100

    Revert "Fifth milestone"

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,15 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(buttonName) {
-    const { total, next, operation } = calculate(this.state, buttonName);
-    this.setState({
-      total,
-      next,
-      operation,
-    });
-  }
-
-  render() {
-    const { next } = this.state;
-    let { total } = this.state;
-    total = total || '0';
-
-    return (
-      <>
-        <div className="container">
-          <Display result={next || total} />
-          <ButtonPanel clickHandler={this.handleClick} />
-        </div>
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <div className="container">
+      <Display result="0" />
+      <ButtonPanel />
+    </div>
+  </>
+);
 
 export default App;
