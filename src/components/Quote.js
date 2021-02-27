32a984370b3da0ea449f469c1961096eb0c5a1bc
Bello S Babakolo <fleetbeekay@gmail.com>
2021-02-27 01:38:20 +0100

    Add style to the quote page

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Quote = () => (
   <div className="content">
-    <p className="quotes">
+    <p
+      className="quotes"
+      style={{
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        position: 'absolute',
+        textAlign: 'center',
+        fontSize: '1.3rem',
+      }}
+    >
       Mathematics is about numbers, equations, computations, or
       algorithms: it is about understanding. -- William Paul Thurston
     </p>
